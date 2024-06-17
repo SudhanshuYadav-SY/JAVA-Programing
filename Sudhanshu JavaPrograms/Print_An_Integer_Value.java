fa8f017ee7e44dc8142057f2517936d9ec3196f2
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-06-17 18:31:40 +0530

    Add files via upload

---
+++ Sudhanshu JavaPrograms/Print_An_Integer_Value.java
@@ -0,0 +1,14 @@
+public class Print_An_Integer_Value
+{
+    public static void main(String[] args)
+    {
+        int a = 10;
+        System.out.println(a);
+        a = 15;
+        System.out.println(a);
+        //final int b = 10;
+        //System.out.println(b);
+        int b = 18;
+        System.out.println(b);
+    }
+}
