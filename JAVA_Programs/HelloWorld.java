d2833f467cdac15cad4712359b16a478cd3572d9
Shivani0318 <153417310+Shivani0318@users.noreply.github.com>
2024-06-23 04:26:45 -0700

    Update HelloWorld.java

--- JAVA_Programs/HelloWorld.java
+++ JAVA_Programs/HelloWorld.java
@@ -2,6 +2,6 @@ class HelloWorld
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World!! My Name is Sudhanshu Yadav And I Love you Shivani");
+		System.out.println("Hello World!! My Name is Sudhanshu Yadav");
 	}
 }
