7be6135e77d08e9444ed0f9d3e538d815fb50193
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-05 00:36:29 +0530

    Add files via upload

--- Sudhanshu JavaPrograms/Check_Leap_Year.java
+++ Sudhanshu JavaPrograms/Check_Leap_Year.java
@@ -1,33 +1,31 @@
 import java.util.Scanner;
 
-public class Check_Leap_Year
-{
-    public static void main(String[] args)
-    {
-      Scanner sc = new Scanner(System.in);
-      System.out.println("Enter a random year of your choice");
-      int year = sc.nextInt();      //Initialise and Assign the value at the same time
-      if (year % 4 == 0)
-      {
-          if (year % 100 == 0)
-          {
-              if (year % 400 == 0)
-              {
-                  System.out.println("Year Entered is a Leap Year");
-              }
-              else
-              {
-                  System.out.println("Year Entered is not a leap Year");
-              }
-          }
-          else
-          {
-              System.out.println("Year Entered is not a leap Year");
-          }
-      }
-      else
-      {
-          System.out.println("Year Entered is not a leap Year");
-      }
+public class Check_Leap_Year {
+    public static void main(String[] args) {
+        Scanner shivani = new Scanner(System.in);
+        System.out.println("Enter a random year of your choice:");
+        int year = shivani.nextInt();                       // Initialize and assign the value at the same time
+        if (year % 4 == 0)
+        {
+            if (year % 100 == 0)
+            {
+                if (year % 400 == 0)
+                {
+                    System.out.println("Year Entered is a Leap Year");
+                }
+                else
+                {
+                    System.out.println("Year Entered is not a Leap Year");
+                }
+            }
+            else
+            {
+                System.out.println("Year Entered is a Leap Year");
+            }
+        }
+        else
+        {
+            System.out.println("Year Entered is not a Leap Year");
+        }
     }
 }
