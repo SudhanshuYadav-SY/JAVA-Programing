8ae2042bb9b6b0386660fcdcebd8b5ad00bc3b79
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-10 00:14:55 +0530

    Add files via upload

--- Sudhanshu JavaPrograms/Typecasting_in_JAVA.java
+++ Sudhanshu JavaPrograms/Typecasting_in_JAVA.java
@@ -2,11 +2,13 @@ public class Typecasting_in_JAVA
 {
     public static void main(String[] args)
     {
-        double a = 9;               //Here data literal is int and data type is 9 so java will automatically convert it to double
+        double a = 9;                           //Here data literal is int and data type is 9 so java will automatically convert it to double
         System.out.println(a);
-        int b = 3.674F;             //Here Data literal is float and data type is int hence type casting will be done
+        int b = (int) 3.674F;                   //Here Data literal is float and data type is int hence type casting will be done
         System.out.println(b);
-        int c = true;               //Here Data literal is boolean and data type is int hence Compiler Error will occur
-        System.out.println(c);
+        /*
+        int c = true;                           //Here Data literal is boolean and data type is int hence Compiler Error will occur
+         System.out.println(c);
+         */
     }
 }
