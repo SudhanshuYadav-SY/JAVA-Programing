dc6bf577975d76970fdf23d37875c615bee50349
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-07-10 01:11:38 +0530

    Add files via upload

--- Sudhanshu JavaPrograms/Methods.java
+++ Sudhanshu JavaPrograms/Methods.java
@@ -2,15 +2,25 @@ public class Methods
 {
     static int a = 9, b = 11,c;
 
-    public static void method_1()
+    public static void display_values()
     {
         System.out.println("a = "+a);
         System.out.println("b = "+b);
     }
-    public static void main(String[] args)
+    public static void sum()
     {
-        method_1();
         c = a + b;
         System.out.println("Sum = "+c);
+    }
+    public static void product()
+    {
+       int prod = a * b ;                               //Here Prod is a method variable
+       System.out.println("Product = "+prod);
+    }
+    public static void main(String[] args)
+    {
+        display_values();                               //Method-Calling Statement
+        sum();                                          //Method-Calling Statement
+        product();                                      //Method-Calling Statement
     }
 }
